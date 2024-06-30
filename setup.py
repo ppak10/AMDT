2d11007c77fa41c4bcbf4557f11db1127e7d5538
Peter Pak <ppak10@gmail.com>
2024-06-30 18:29:04 -0400

    Uploads version 0.0.2 of AMDT.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     author="Peter Pak",
     name="amdt",
-    version="0.1",
+    version="0.0.2",
     # Loads in local packages
     packages=find_packages(),
 )
